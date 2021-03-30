4245003f62981408b4adaf50fd68f635917d0685
Florian Bacher <florian.bacher@dynatrace.com>
2021-03-30 09:01:04 +0200

    added timestamp and input event
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- shipyard-controller/handler/tasksequence_state.go
+++ shipyard-controller/handler/tasksequence_state.go
@@ -3,6 +3,7 @@ package handler
 import (
 	keptnapimodels "github.com/keptn/go-utils/pkg/api/models"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
+	"time"
 )
 
 type TaskSequenceStatus string
@@ -31,6 +32,8 @@ type TaskState struct {
 
 type TaskSequenceState struct {
 	Status        TaskSequenceStatus
+	Timestamp     time.Time
+	InputEvent    keptnapimodels.KeptnContextExtendedCE // event that triggered the task sequence
 	KeptnContext  string
 	EventScope    keptnv2.EventData // project, stage, service, labels
 	Shipyard      keptnv2.Shipyard  // in case the shipyard changes during the task sequence execution, keep it in the state
